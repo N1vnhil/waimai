ddd2c4ffac286c457d285b12372830ef9fd53ae1
n1nvhil <n1vnhil@outlook.com>
2025-02-13 15:56:35 +0800

    购物车相关接口

--- server/sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ server/sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,41 +1,29 @@
 package com.sky.controller.user;
 
+
 import com.sky.dto.ShoppingCartDTO;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
-@Api(tags = "C端购物车相关接口")
 @RestController
+@Slf4j
+@Api(tags = "购物车相关接口")
 @RequestMapping("/user/shoppingCart")
 public class ShoppingCartController {
 
     @Autowired
     private ShoppingCartService shoppingCartService;
 
+    @ApiOperation("添加商品")
     @PostMapping("/add")
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
-        log.info("添加购物车，商品信息：{}", shoppingCartDTO);
+        log.info("加入购物车：{}", shoppingCartDTO);
         shoppingCartService.addShoppingCart(shoppingCartDTO);
-        return Result.success();
-    }
-
-    @GetMapping("/list")
-    public Result list() {
-        return Result.success();
-    }
-
-    @DeleteMapping("/clean")
-    public Result clean() {
-        return Result.success();
-    }
-
-    @PostMapping("/sub")
-    public Result sub() {
         return Result.success();
     }
 
